b46d57dc1f8b0e222f2119460ebf67e85848cfbf
yogiyulianto <yogi.yulianto@students.amikom.ac.id>
2018-05-25 19:45:42 +0700

    first commit

---
+++ app/src/main/java/com/bringin/iak/amikomwebview/MainActivity.java
@@ -0,0 +1,25 @@
+package com.bringin.iak.amikomwebview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.web_view);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        webView.loadUrl("https://itc.amcc.or.id/");
+    }
+}
